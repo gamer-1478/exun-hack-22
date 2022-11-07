4d3d209b00b6fd65bb30c8ffbb9fc7e19db3f7b2
AlightKnight343 <thegodofgreatness343@gmail.com>
2022-11-07 16:56:11 +0530

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+    session = require('cookie-session'),
+    passport = require('passport')
+
+
+//file imports
+const landing = require('./routes/landing')
+
+//middlewares
+app.use(express.json({ limit: '1mb' }), express.urlencoded({ extended: true, limit: '1mb' }))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+}))
+
+//connect to mongodb
+// const dbUri = process.env.MONGO_URI
+// mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("Connected to mongodb"))
+
+//initialize passport after this
+
+
+//routing
+app.get('/', landing)
+
+//listen
+const PORT = 8080 || process.env.PORT
+app.listen(PORT, ()=> console.log(`Connected on port ${PORT}`))
