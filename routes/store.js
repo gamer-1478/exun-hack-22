c0aa7ed314022c5a9631ddc2d27bfcdce5c3096d
AlightKnight343 <thegodofgreatness343@gmail.com>
2022-11-08 11:50:01 +0530

    huh

---
+++ routes/store.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const Game = require('../schemas/gameSchema');
+
+router.get('/', async (req, res)=> {
+    try{
+        const games = await Game.find({})
+        res.send(games)
+    }catch(err){
+        res.send({"msg":`${err}`})
+    }
+})
+
+router.get('/:gameId', async(req, res)=>{
+    const id = req.params.gameId 
+    try{
+        const game = await Game.findOne({id:id})
+        if(!game){
+            res.send({"msg": "No Game Found!"})
+        }else{
+            res.send(game)
+        }
+    }catch(err){
+        res.send({"msg":`${err}`})
+    }
+})
+
+module.exports = router;
