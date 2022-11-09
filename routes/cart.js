24dfd1d81ddface65833fb1fb9fa72f3311e55cc
Miss Bisht <73991231+Psych4-383@users.noreply.github.com>
2022-11-09 14:08:29 +0530

    :checkout

--- routes/cart.js
+++ routes/cart.js
@@ -18,7 +18,9 @@ router.get('/', (req, res) => {
         return product;
     })
     Promise.all(products).then(prods => {
-        res.render("cart", { user: req.user, cart: products, total: total})
+        req.user.total = total;
+        req.user.save()
+        res.render("cart", { user: req.user, cart: products, total: total })
     }).catch(err => {
         console.log(err)
     })
@@ -31,7 +33,7 @@ router.get('/add/:id', ensureAuthenticated, (req, res) => {
     cart.push(store_id);
     req.user.cart = cart
     req.user.save();
-    res.send({msg: "Added to cart"})
+    res.send({ msg: "Added to cart" })
 })
 
 router.post('/delete/:id', ensureAuthenticated, (req, res) => {
@@ -44,43 +46,49 @@ router.post('/delete/:id', ensureAuthenticated, (req, res) => {
     res.send({ success: true });
 })
 
-router.get('/checkout-confirm', async (req, res) => { //  need total and products from form
-
-
-    //create price obejct
+router.get('/checkout-confirm', ensureAuthenticated, async (req, res) => { // need total
+    //create price object
     var price_id;
     stripe.products.create({
         name: 'Cart Checkout',
         description: 'Payment',
     }).then(prod => {
+        console.log(prod.id)
         stripe.prices.create({
-            unit_amount: req.body.total,
+            unit_amount: req.user.total,
             currency: 'inr',
-            prod: prod.id,
+            product: prod.id,
         }).then(price => {
             price_id = price.id;
         });
     });
-
+    const successUrl = process.env.HOSTNAME + '/cart/success';
+    const cancelUrl = process.env.HOSTNAME + '/cart';
+    console.log(successUrl)
     // checkout session
     const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price: price_id,
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `/checkout-success`,
-      cancel_url: `/`,
+        line_items: [
+            {
+                price: price_id,
+                quantity: 1,
+                price_data: {currency:'inr'},
+            },
+        ],
+        mode: 'payment',
+        success_url: successUrl,
+        cancel_url: cancelUrl,
     });
+    res.redirect(303, session.url);
+});
 
+
+router.get('/success', (req, res) => {
     //adding games to library
     var lib = req.user.library;
-    lib.push(req.products)
-    req.user.library = lib
-    req.user.save()
-    res.redirect(303, session.url);
-  });
-
+    lib.push(req.user.cart)
+    req.user.cart = [];
+    req.user.library = lib;
+    req.user.save();
+    res.render('')
+})
 module.exports = router;
