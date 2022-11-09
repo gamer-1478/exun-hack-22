04c799593e4f9ba2413d1b6dd9d0269476f9e280
Miss Bisht <73991231+Psych4-383@users.noreply.github.com>
2022-11-09 14:54:39 +0530

    total cost handling added

--- routes/cart.js
+++ routes/cart.js
@@ -29,7 +29,6 @@ router.get('/add/:id', ensureAuthenticated, async (req, res) => {
     const store_id = req.params.id;
     var product = await Game.findOne({ id:store_id}) || await Asset.findOne({ id:store_id})
     req.user.total+=product.cost
-
     var cart = req.user.cart;
     cart.push(store_id);
     req.user.cart = cart
@@ -37,25 +36,32 @@ router.get('/add/:id', ensureAuthenticated, async (req, res) => {
     res.send({ msg: "Added to cart" })
 })
 
-router.post('/delete/:id', ensureAuthenticated, (req, res) => {
+router.post('/delete/:id', ensureAuthenticated, async (req, res) => {
     var id = req.params.id;
     var cart = req.user.cart;
     var index = cart.findIndex(product => product.id === id);
     cart.splice(index, 1);
+    var product = await Game.findOne({ id:store_id}) || await Asset.findOne({ id:store_id})
+    req.user.total-=product.cost
     req.user.cart = cart;
     req.user.save();
     res.send({ success: true });
 })
 
-router.get('/checkout-confirm', ensureAuthenticated, async (req, res) => { // need total
+router.get('/checkout-confirm', ensureAuthenticated, async (req, res) => {
     const successUrl = process.env.HOSTNAME + '/cart/success';
     const cancelUrl = process.env.HOSTNAME + '/cart';
+    console.log(req.user.total)
+    if (req.user.total==0){
+        res.redirect(cancelUrl)
+        return;
+    }
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {   
                 quantity: 1,
                 price_data: {
-                        unit_amount: req.user.total,
+                        unit_amount: req.user.total*100,
                         currency: 'inr',
                         product_data: {
                             name: 'Cart Checkout',
@@ -69,17 +75,15 @@ router.get('/checkout-confirm', ensureAuthenticated, async (req, res) => { // ne
         success_url: successUrl,
         cancel_url: cancelUrl,
     });
+    var lib = req.user.library;
+    console.log(lib)
+    lib = lib + (req.user.cart)
+    req.user.library = lib;
+    console.log(lib, req.user.library)
+    req.user.cart = [];
+    req.user.total = 0;
+    console.log(req.user.total)
+    req.user.save();
     res.redirect(303, session.url);
 });
-
-
-router.get('/success', (req, res) => {
-    //adding games to library
-    var lib = req.user.library;
-    lib.push(req.user.cart)
-    req.user.cart = [];
-    req.user.library = lib;
-    req.user.save();
-    res.render('')
-})
 module.exports = router;
